2afdd0c3caa03c045b4a383a2f99c2d4b3131bdd
Alex <thoms.alexander23@gmail.com>
2021-05-27 15:46:12 -0400

    Feat: done

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -6,14 +6,9 @@ import java.io.IOException;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/count")
     public class CounterServlet extends HttpServlet {
-
         private int counter = 0;
-
         protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, IOException {
-            counter += 1; // counter = counter + 1;
-
+            counter += 1;
             res.getWriter().println("<h1>This site has been viewed: " + counter + " times!<h1>");
-
         }
     }
-
