3cb2cb7009c067ee8d2e7aa40406d73b750b6916
Alex <thoms.alexander23@gmail.com>
2021-06-02 12:34:24 -0400

    Initial Commit: Started Exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,12 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        if (request.getAttribute("user").equals("admin")){
+            request.getRequestDispatcher("profile.jsp").forward(request, response);
+
+        }
+        else {
+            response.sendRedirect("/login");
+        }
     }
 }
